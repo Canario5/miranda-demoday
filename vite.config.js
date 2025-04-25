79dc05a8cb76b0c1693d8e1ad161a553aec90162
Canario5 <48844307+Canario5@users.noreply.github.com>
2025-04-25 14:19:52 +0200

    chore: Add vite-plugin-svg-spritemap, SVGO, prettier

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import VitePluginSvgSpritemap from "@spiriit/vite-plugin-svg-spritemap";
+
+export default defineConfig({
+  plugins: [
+    VitePluginSvgSpritemap("./assets/img/icons/*.svg", {
+      styles: {
+        filename: "assets/scss/spritemap.scss",
+        format: "scss",
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                // Convert colors to currentColor for CSS styling
+                convertColors: {
+                  currentColor: true,
+                },
+                // Remove unnecessary attributes
+                removeViewBox: false,
+              },
+            },
+          },
+          // Additional plugins can be added here
+          {
+            name: "removeStyleElement",
+          },
+        ],
+      },
+    }),
+  ],
+});
