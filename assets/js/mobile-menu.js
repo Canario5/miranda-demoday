37d3e12e4b0f91eec37bb4f88c8e1adf1320efed
Canario5 <48844307+Canario5@users.noreply.github.com>
2025-05-12 16:13:53 +0200

    feat(mobile-menu): Improve mobile menu accessibility

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -2,15 +2,31 @@ const initMobileMenu = () => {
   const menuToggleButton = document.querySelector(".js-mobile-menu-toggle");
   const mainNavigation = document.querySelector(".js-main-navigation");
 
-  if (menuToggleButton && mainNavigation) {
-    menuToggleButton.addEventListener("click", () => {
-      document?.body.classList.toggle("mobile-menu-active");
-      menuToggleButton.classList.toggle("is-active");
+  if (!menuToggleButton || !mainNavigation) return;
 
-      const isExpanded =
-        menuToggleButton.getAttribute("aria-expanded") === "true";
-      menuToggleButton.setAttribute("aria-expanded", String(!isExpanded));
-      mainNavigation.setAttribute("aria-hidden", String(isExpanded));
+  const navLinks = mainNavigation.querySelectorAll("a[href]");
+  let isMenuOpen = mainNavigation.getAttribute("aria-hidden") === "false";
+  updateMenuAccessibility(isMenuOpen);
+
+  menuToggleButton.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
+    updateMenuVisuals(isMenuOpen);
+    updateMenuAccessibility(isMenuOpen);
+  }
+
+  function updateMenuVisuals(isOpen) {
+    document.body.classList.toggle("mobile-menu-active", isOpen);
+    menuToggleButton.classList.toggle("is-active", isOpen);
+  }
+
+  function updateMenuAccessibility(isOpen) {
+    menuToggleButton.setAttribute("aria-expanded", String(isOpen));
+    mainNavigation.setAttribute("aria-hidden", String(!isOpen));
+
+    navLinks.forEach((link) => {
+      link.setAttribute("tabindex", isOpen ? "0" : "-1");
     });
   }
 };
